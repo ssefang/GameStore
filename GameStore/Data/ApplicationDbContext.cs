257a9382ebec3930ed8975b5e2498fc9f6a37ce1
ssefang <91700513+ssefang@users.noreply.github.com>
2022-08-29 21:36:34 -0400

    add

---
+++ GameStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using GameStore.Models;
+
+namespace GameStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categorys { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ GameStore/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace GameStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+     
+        
+        
+        [Required]
+        [Range(1, 10000)]
+        [Display(Name = "Price")]
+        public double Price { get; set; }
+         
+       
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+
+      
+    }
+}
